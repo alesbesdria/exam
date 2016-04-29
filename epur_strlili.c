32aa2860c95aea5f9a4cade9a7476900f8e4d429
alesbesdria <melissa@meirsman.fr>
2016-04-29 15:45:02 +0200

    epurstr

--- epur_strlili.c
+++ epur_strlili.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 
-int			ft_strlen(char *s)
+int		ft_strlen(char *s)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while (s[i] != '\0')
@@ -10,38 +10,42 @@ int			ft_strlen(char *s)
 	return (i);
 }
 
-int			main(int argc, char ** argv)
+int		main(int argc, char **argv)
 {
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 
 	i = 0;
+	j = 0;
 	if (argc != 2)
 	{
-		write(1, "\n", 1);
+		write(1, "\n",1);
 		return (0);
-	}	
-	while ((argv[1][i] == 9 || argv[1][i] == 10 || argv[1][i] == 11
-				|| argv[1][i] == 32) && argv[1][i] != '\0')
-		i++;
-	j = ft_strlen(argv[1]) - 1;
-	while (argv[1][i] != '\0' && argv[1][i] < argv[1][j])
-	{
-		j = ft_strlen(argv[1]) - 1;
-		while (argv[1][j] <= 126 && argv[1][j] >= 33)
-			j--;
-		while (argv[1][i] <= 126 && argv[1][i] >= 33)
-		{
-			write(1, &argv[1][i], 1);
-			i++;
-		}
-		if (argv[1][i] == 9 || argv[1][i] == 10 || argv[1][i] == 11 
-				|| argv[1][i] == 32)
-			write(1, " ", 1);
-		while (argv[1][i] >= 126 && argv[1][i] <= 33)
-			i++;
 	}
+	while (argv[1][i] == 9 || argv[1][i] == 32)
+		i++;
+	while (argv[1][i] != '\0')
+	{
+		if (argv[1][i] == 9 || argv[1][i] == 32)
+		{
+			argv[1][j] = 32;
+			i++;
+			j++;
+			while (argv[1][i] == 9 || argv[1][i] == 32)
+				i++;
+		}
+		else
+			while (argv[1][i] != 9 && argv[1][i] != 32 && argv[1][i] != '\0')
+			{
+				argv[1][j] = argv[1][i];
+				j++;
+				i++;
+			}
+	}
+	if (argv[1][j - 1] == 32)
+		j--;
+	argv[1][j] = '\0';
+	write(1, argv[1], ft_strlen(argv[1]));
 	write(1, "\n", 1);
 	return (0);
 }
-
